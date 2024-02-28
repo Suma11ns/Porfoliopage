3cd40d9c0f91bf98ab5a1f8e7bf377bd5d9e339a
Suma11ns <149225833+Suma11ns@users.noreply.github.com>
2024-02-28 18:05:11 +0530

    Add files via upload

---
+++ src/components/AboutContent.js
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom"
+import "./aboutContentstyles.css"
+import React1 from "../assets/pic1.jpg"
+import React2 from "../assets/pic2.jpg"
+
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Techical Skills</h1>
+            <p>Java,SQL</p>
+            <p>Javascript</p>
+            <p>HTML,CSS</p>
+            <p>React Redux</p>
+            <p>Nodejs</p>
+            <p>Expressjs</p>
+            <p>MongodDB</p>
+            <p>AWS</p>
+            <Link to ="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={React2} className="img" alt="true"/>
+
+                </div>
+                <div className="img-stack bottom">
+                    <img src={React1} className="img" alt="true"/>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default AboutContent
---
+++ src/components/workCardData.js
@@ -0,0 +1,61 @@
+import pro1 from "../assets/landingpage.jpg"
+import pro2 from "../assets/movierec.jpg"
+import pro3 from "../assets/timer.JPG"
+import pro4 from "../assets/snakegame.jpg"
+import pro5 from "../assets/calculator.jpg"
+import pro6 from "../assets/quizegame.jpeg"
+import pro7 from "../assets/portfolio.jpg"
+
+
+
+
+const ProjectCardData=[
+    {
+        imgsrc: pro1,
+        title:"Landing Page",
+        text:"Basic langing page which can be used for company or product which includes header,footer and some basic information",
+        view:"url here",
+        source:"url here"
+    },
+    {
+        imgsrc: pro2,
+        title:"Movie Recommendation",
+        text:"Proposed proejct can recommend a Movie or TV show by comination of attributes",
+        view:"https://github.com/Suma11ns/MoviesRecommendation",
+        source:"https://github.com/Suma11ns/MoviesRecommendation"
+    },
+    {
+        imgsrc: pro3,
+        title:"Countdown timer",
+        text:"Countdown timer that counts down from a set time to zero,with options to stop,reset and restart the timer",
+        view:"url here"
+    },
+    {
+        imgsrc: pro4,
+        title:"Snake game",
+        text:"Allow the player to control the snakes movement using keyboard inputs,typically arrow keys.Provides smooth gaming experience",
+        view:"url here"
+    },
+    {
+        imgsrc: pro5,
+        title:"Calculator",
+        text:"Calculator app which performs basic math operations",
+        view:"url here"
+    },
+    {
+        imgsrc: pro6,
+        title:"Quiz game",
+        text:"Developed with aim of user-friendly interface for answering quiz questions,calculates users score",
+        view:"url here"
+    },
+    {
+        imgsrc: pro7,
+        title:"",
+        text:"My portfolio website is visual hub where i showcase my diverse skills,projects and my best work",
+        view:"url here"
+    },
+    
+    
+    
+];
+export default ProjectCardData;
